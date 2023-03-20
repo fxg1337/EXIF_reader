bee90e0e27619bca94c2f2dc5db869f4dd2a749a
fxg1337 <104424535+fxg1337@users.noreply.github.com>
2023-03-20 15:44:12 +0000

    Create setup.py

---
+++ .github/workflows/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'EXIF_reader',
+    packages = ['EXIF_reader'],
+    version = 'version number',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = '',
+    author_email = '',
+    url = 'https://github.com/fxg1337/EXIF_reader',
+    download_url = 'download link you saved',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
